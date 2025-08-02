f3957ee3b6f0410e6801f35d546541a7062de8a6
mucy <895179967@qq.com>
2025-08-02 18:38:28 +0800

    commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,8 +6,8 @@ module.exports = {
   solidity: "0.8.28",
   networks: {
     sepolia: {
-      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [process.env.PK],
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.PK, process.env.PK2],
     },
   },
   namedAccounts: {
